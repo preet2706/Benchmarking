dd31f72fc7d582b62bbcb9e8b16954b4fa768432
preet2706 <preetnshah27@gmail.com>
2025-06-03 11:35:56 +0200

    started atac analysis; finished function for deg

--- metrics.py
+++ metrics.py
@@ -9,6 +9,7 @@ import zlib
 import base64
 import seaborn as sns
 import matplotlib.pyplot as plt
+import gseapy as gp
 
 def run_pyscenic(path_adata, data_dir, model:str):
     os.chdir(data_dir)
@@ -87,3 +88,96 @@ def bootstrap_auc(input_file:str, data_dir:str, n_iterations:int = 50, seed:int 
         auc_mtx2 = compute_auc(input_path=input_file, data_dir=data_dir, output_file_name='bootstrap_auc.loom', loom_file_name='')
         grcs[i] = compute_grcs(auc_mtx, auc_mtx2)[0]
     return {'mean': np.mean(grcs), 'std': np.std(grcs)}   
+
+def compute_deg(RNA_data, hv=False, plot=False):
+    RNA_data.obs['group'] = RNA_data.obs['Cell Types'].map(
+    lambda x: 'B' if x in ['B'] else ('LYM' if x in ['Tumor B', 'Tumor B cycling'] else None)
+        )
+    data_gsea = RNA_data[RNA_data.obs['group'].isin(['B','LYM'])].copy()
+
+    if hv is True:
+        data_gsea = data_gsea[:, data_gsea.var['highly_variable']]
+
+    sc.pp.log1p(data_gsea)
+    sc.tl.rank_genes_groups(
+    data_gsea,
+    groupby='group',
+    groups=['LYM'],
+    reference='B',
+    method='wilcoxon',
+    pts=True
+    )
+
+    deg = sc.get.rank_genes_groups_df(data_gsea, group='LYM')
+    deg['neg_log10_padj'] = -np.log10(deg['pvals_adj'].replace(0, np.nan))
+
+    deg_list = deg[(deg["pvals_adj"] < 0.05) & (abs(deg["logfoldchanges"]) > 1)]['names'].tolist()
+
+    enr = gp.enrichr(
+    gene_list=deg_list,
+    gene_sets='MSigDB_Hallmark_2020',  # or 'GO_Biological_Process_2021', 'Reactome_2022', etc.
+    organism='Human',
+    background=list(data_gsea.var_names),          
+    outdir=None,     # optional
+    cutoff=0.01                   # p-value cutoff
+    )
+
+    sig_enrichments = enr.results[enr.results['Adjusted P-value'] < 0.05]
+
+    if plot is True:  
+        df = deg.copy()  
+        df['color'] = 'grey'
+        df.loc[(df['logfoldchanges'] > 1) & (df['pvals_adj'] < 0.001), 'color'] = 'red'
+        df.loc[(df['logfoldchanges'] < -1) & (df['pvals_adj'] < 0.001), 'color'] = 'blue'
+
+        plt.figure(figsize=(7, 6))
+        plt.scatter(df['logfoldchanges'], df['neg_log10_padj'], s=10, alpha=0.7, c=df['color'], edgecolor='none')
+        plt.axhline(-np.log10(0.001), color='green', linestyle='--')
+        plt.axvline(1, color='green', linestyle='dotted')
+        plt.axvline(-1, color='green', linestyle='dotted')
+
+        sig_df = df[df['pvals_adj'] < 0.001]
+        top_up = sig_df.sort_values(by='logfoldchanges', ascending=False).head(10)
+        top_down = sig_df.sort_values(by='logfoldchanges', ascending=True).head(10)
+
+        for _, row in pd.concat([top_up, top_down]).iterrows():
+            plt.text(row['logfoldchanges'], row['neg_log10_padj'],
+                    row['names'], fontsize=7, ha='center', va='bottom', color='black')
+
+        plt.xlabel('Log2 FC')
+        plt.ylabel('-Log10 Adj_p')
+        plt.title('Volcano Plot: Lymphoma B vs B Cells')
+        plt.tight_layout()
+        plt.savefig('my_plot.svg', format='svg')
+
+    return sig_enrichments
+
+    def compute_pathway_overlap(true_df, pred_df, pval_thresh=0.05):
+    # Identify common enriched terms
+    shared_terms = set(sig_true['Term']).intersection(sig_pred['Term'])
+
+    dict = {}
+    jaccards = 0.0
+    for term in shared_terms:
+        # Get gene sets for the term from both dataframes
+        genes_true = set(sig_true[sig_true['Term'] == term]['Genes'].values[0].split(';'))
+        genes_pred = set(sig_pred[sig_pred['Term'] == term]['Genes'].values[0].split(';'))
+
+        # Compute Jaccard index (overlap)
+        union = genes_true.union(genes_pred)
+        if len(union) == 0:
+            continue  # avoid division by zero
+        jaccard = len(genes_true.intersection(genes_pred)) / len(union)
+        jaccards += jaccard
+        fp = len(genes_pred - genes_true)
+        fn = len(genes_true - genes_pred)
+        dict[term] = {
+            'Jaccard': jaccard,
+            'FP': fp,
+            'FN': fn,
+        }
+
+    if jaccards > 0:
+        return jaccards / len(dict.values()), dict 
+    else:
+        return 0.0, dict  # or np.nan if you prefer
